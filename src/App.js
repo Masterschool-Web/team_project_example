3f63d4662bc763cb0675754fd147872c3e74cc49
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-11-16 20:21:50 +0100

    register works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
+import { useAuth } from "./context/authContext";
 
 function App() {
+  const { user } = useAuth();
+
+  if (user) return <div>{user.email}</div>;
   return <div className='App'>Hello World</div>;
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import AuthProvider from "./context/authContext";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "./pages/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
