7bb2b9b32a570e09bc1995a3ebc0127c5077f904
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-11-17 20:12:00 +0100

    feat[login]: links to signup and home

---
+++ src/pages/Login.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/authContext";
+import { processFirebaseErrors } from "../firebase/errors";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      // go to db
+      await login({ email, password });
+      setLoading(false);
+
+      // navigate to a different page
+      navigate("/");
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+      setError(processFirebaseErrors(err.message));
+    }
+  };
+
+  if (loading) return <div>loading...</div>;
+
+  return (
+    <>
+      <Link to='/'>Back</Link>
+      <form onSubmit={onSubmit}>
+        <h1>Login</h1>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <label>Email</label>
+        <input
+          type='text'
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <label>Password</label>
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <input type='submit' value='SUBMIT' />
+      </form>
+      <p>
+        New to our website? <Link to='/register'>Signup</Link>
+      </p>
+    </>
+  );
+};
+
+export default Login;
